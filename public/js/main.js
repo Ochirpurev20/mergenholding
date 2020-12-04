a0cb58151fa33d1eda69f146aea74e299fb124bf
ochiroo <oscaravna2020@gmail.com>
2020-12-04 13:50:09 +0800

    slider fixing

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,22 @@
-const imgDir = './img/'
-let currentImgIndex = 2;
-const slide = (n) => {
-    currentImgIndex = currentImgIndex + n
-    console.log('curr ==', currentImgIndex)
-    if(currentImgIndex >0  && currentImgIndex <5) {
-        document.getElementById('current-image').src = imgDir+'img'+currentImgIndex+'.jpg'
-    } else {
-        alert('press next button and cur ==='+currentImgIndex)      
-        
-    }
+const imgDir = "./img/";
+let currentImgIndex = 1;
+let lastIndex = 0;
+document.addEventListener("DOMContentLoaded", (e) => {
+  slide(1);
+});
+
+function slide(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  currentImgIndex = currentImgIndex + n;
+  // console.log('curr ==', currentImgIndex)
+  if (currentImgIndex > 0 && currentImgIndex < 5) {
+    // console.log('slides ==',slides)
+    slides[lastIndex].style.display = "none";
+    slides[currentImgIndex - 1].style.display = "block";
+    lastIndex = currentImgIndex - 1;
+  } else if (currentImgIndex > 4) {
+    currentImgIndex = 0;
+  } else {
+    currentImgIndex = 4;
+  }
 }
